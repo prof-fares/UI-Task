8b9eef91ddfe48b292bbeb58a171b13163e4123d
prof-fares <Fareselkg@gmail.com>
2024-11-02 20:22:12 +0200

    fix missing use App sizes resource

--- lib/presentation/resourceManger/app_styles.dart
+++ lib/presentation/resourceManger/app_styles.dart
@@ -1,54 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:task_ui_challenge/presentation/resourceManger/app_colors.dart';
+import 'package:task_ui_challenge/presentation/resourceManger/app_sizes.dart';
 import 'package:task_ui_challenge/presentation/resourceManger/app_weights.dart';
 
 class AppStyles {
   static TextStyle normalBlackStyle(BuildContext context) => TextStyle(
         fontWeight: AppWeights.w500,
-        fontSize: 12.sp,
+        fontSize: AppSizes.s12.sp,
         color: Theme.of(context).textTheme.bodyLarge?.color ?? AppColors.myBlack,
       );
 
   static TextStyle boldBlackStyle(BuildContext context) => TextStyle(
         fontWeight: AppWeights.w800,
-        fontSize: 14.sp,
+        fontSize: AppSizes.s14.sp,
         color: Theme.of(context).textTheme.bodyLarge?.color ?? AppColors.myBlack,
       );
 
   static TextStyle bigBoldBlackStyle(BuildContext context) => TextStyle(
         fontWeight: AppWeights.w800,
-        fontSize: 18.sp,
+        fontSize: AppSizes.s18.sp,
         color: Theme.of(context).textTheme.bodyLarge?.color ?? AppColors.myBlack,
       );
 
   static TextStyle normalBlueStyle(BuildContext context) => TextStyle(
         fontWeight: AppWeights.w500,
-        fontSize: 12.sp,
+        fontSize: AppSizes.s12.sp,
         color: Theme.of(context).primaryColor,
       );
 
   static TextStyle semiBoldBlueStyle(BuildContext context) => TextStyle(
         fontWeight: AppWeights.w800,
-        fontSize: 10.sp,
+        fontSize: AppSizes.s10.sp,
         color: Theme.of(context).primaryColor,
       );
 
   static TextStyle boldBlueStyle(BuildContext context) => TextStyle(
         fontWeight: AppWeights.w800,
-        fontSize: 14.sp,
+        fontSize: AppSizes.s14.sp,
         color: Theme.of(context).primaryColor,
       );
 
   static TextStyle normalWhiteStyle(BuildContext context) => TextStyle(
         fontWeight: AppWeights.w400,
-        fontSize: 12.sp,
+        fontSize: AppSizes.s12.sp,
         color: Theme.of(context).scaffoldBackgroundColor,
       );
 
   static TextStyle boldWhiteStyle(BuildContext context) => TextStyle(
         fontWeight: AppWeights.w500,
-        fontSize: 16.sp,
+        fontSize: AppSizes.s16.sp,
         color: Theme.of(context).scaffoldBackgroundColor,
       );
 }
